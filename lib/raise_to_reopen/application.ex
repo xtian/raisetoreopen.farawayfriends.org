2c799f799eadb6bde6fd02baedc35433000a21be
Christian Wesselhoeft <hi@xtian.us>
2020-11-15 06:41:38 -0700

    Remove boilerplate

--- lib/raise_to_reopen/application.ex
+++ lib/raise_to_reopen/application.ex
@@ -1,24 +1,15 @@
 defmodule RaiseToReopen.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Telemetry supervisor
       RaiseToReopenWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: RaiseToReopen.PubSub},
-      # Start the Endpoint (http/https)
       RaiseToReopenWeb.Endpoint
-      # Start a worker by calling: RaiseToReopen.Worker.start_link(arg)
-      # {RaiseToReopen.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: RaiseToReopen.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- lib/raise_to_reopen_web/endpoint.ex
+++ lib/raise_to_reopen_web/endpoint.ex
@@ -10,10 +10,6 @@ defmodule RaiseToReopenWeb.Endpoint do
     signing_salt: "rPhD9xpO"
   ]
 
-  socket "/socket", RaiseToReopenWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
